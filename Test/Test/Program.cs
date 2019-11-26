1e3460fb90fdde107ca9175d335d342412ab3944
jakepalmer62 <57322297+jakepalmer62@users.noreply.github.com>
2019-11-26 14:22:23 +0000

    BMI Edit

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -14,19 +14,19 @@ namespace Test
             Console.WriteLine("Your name is");
 
             double weight;
-            Console.WriteLine("How much do you weigh in kilograms?"); 
-            weight = Convert.ToDoubleConsole.ReadLine();
+            Console.WriteLine("How much do you weigh in kilograms?");
+            weight = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("You weigh");
 
             double height;
             Console.WriteLine("What is your height in metres^2?");
-            height = Convert.ToDouble Console.ReadLine(); 
+            height = Convert.ToDouble(Console.ReadLine()) 
             Console.WriteLine("Your height is");
 
 
             double BMI;
             Console.WriteLine("Your BMI is");
-            BMI = Console.ReadLine(height/weight);
+            BMI = Console.WriteLine(height/weight);
 
 
 
