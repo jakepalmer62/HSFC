a845ae87d4dde65bd90afd999b894dfd59640793
jakepalmer62 <57322297+jakepalmer62@users.noreply.github.com>
2019-12-17 14:54:27 +0000

    f

--- QueueProject/QueueProject/Program.cs
+++ QueueProject/QueueProject/Program.cs
@@ -6,41 +6,41 @@ namespace QueueProject
     {
         static void Main(string[] args)
         {
-            Queue Q = new Queue(10);
-
+            Queue queue = new Queue(10);
+            bool running;
             Console.WriteLine("Select which process you would like to run by typing in it's number. The maximum size of the queue is 10");
+            running = true;
 
-            Console.WriteLine("1. Add Person");
-            Console.WriteLine("2. Remove Person");
-            Console.WriteLine("3. View Queue Length");
-            Console.WriteLine("4. Display Queue");
-            Console.WriteLine("5. Quit");
-            Console.ReadLine();
-
+            while (running == true)
+            {
+                Console.WriteLine("1. Add Person");
+                Console.WriteLine("2. Remove Person");
+                Console.WriteLine("3. View Queue Length");
+                Console.WriteLine("4. Display Queue");
+                Console.WriteLine("5. Quit");
+                Console.ReadLine();
+            }
+            
+            
             switch (Console.ReadLine())
             {
-                case "1":
+                case "1": //Enqueue
                     {
-
-
-
+                        Console.WriteLine("Type what you want to add to queue");
+                        queue.EnQueue(Console.ReadLine());
                         break;
                     }
 
-                case "2":
+                case "2": //Dequeue
                     {
-
-
-
+                        Console.WriteLine("Type what you need to remove from the queue");
+                        queue.Dequeue(Console.ReadLine());
                         break;
                     }
 
                 case "3":
                     {
-
-
-
-
+                        Console.WriteLine(queue.Length);
                         break;
                     }
 
@@ -56,9 +56,9 @@ namespace QueueProject
                 case "5":
                     {
 
-
-
-
+                        running = false;
+                        Console.WriteLine("Press enter to exit program");
+                        Console.ReadKey();
                         break;
                     }
             }        
--- QueueProject/QueueProject/Queue.cs
+++ QueueProject/QueueProject/Queue.cs
@@ -4,33 +4,34 @@ using System.Text;
 
 namespace QueueProject
 {
-    class Queue
+    public class Queue
     {
         int QueueLength;
         int Front = 0;
         int Back = 0;
-        public Object[] QueueItems;
+        public readonly Object[] queue;
         private int MaxSize = 10;
 
         public Queue(int QueueSize)
         {
             MaxSize = QueueSize;
-            QueueItems = new Object[QueueSize];
+            queue = new Object[QueueSize];
         }
-        public void EnQueue(Object theObject)
+        public void EnQueue(Queue theQueue)
         {
             if (QueueLength > MaxSize)
             {
-                QueueItems[Back] = theObject;
+                queue[Back] = theQueue;
                 Back = Back + 1;
                 QueueLength++;
                
             }
         }
-        public void DeQueue(Object theObject)
+        public void DeQueue(Queue theQueue)
         {
             if (QueueLength > 0)
             {
+                object found = queue[0];
                 Front = Front + 1 % MaxSize;
                 QueueLength--;
             }
