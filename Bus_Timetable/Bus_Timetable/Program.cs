b1e25218bc5b153dfba6b43764b9fbfec21775e2
jakepalmer62 <57322297+jakepalmer62@users.noreply.github.com>
2020-02-10 21:11:07 +0000

    iaufpweup

--- Bus_Timetable/Bus_Timetable/Program.cs
+++ Bus_Timetable/Bus_Timetable/Program.cs
@@ -13,7 +13,7 @@ namespace Bus_Timetable
     {
         static void Main(string[] args)
         {
-            string fileContent = Resources.data;
+            String fileContent = Resources.data;
             StringReader reader = new StringReader(fileContent);
             String line;
             String[] theDictionary = new String[100];
@@ -35,17 +35,17 @@ namespace Bus_Timetable
             
             Console.WriteLine(theText);
             Console.WriteLine("What Bus are you looking for? Please enter it exactly as it is shown.");
-            string itemToSearchFor = Console.ReadLine();
-
+            String itemToSearchFor = Console.ReadLine();
 
             int result = mySearching.serialSearch(theDictionary, itemToSearchFor);
             if (result == -1)
             {
-                Console.WriteLine("String not Found");
+                Console.WriteLine("Bus not found.");
             }
+
             else
             {
-                Console.WriteLine("String found at: ");
+                Console.WriteLine("Bus found at position: ");
                 Console.WriteLine(result.ToString());
             }
             Console.WriteLine("Thanks for using the bus timetable program, press any key to exit.");
