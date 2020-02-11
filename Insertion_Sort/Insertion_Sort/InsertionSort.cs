9e380e16a4db2476f65b153afcc9dd83f33e59a8
jakepalmer62 <57322297+jakepalmer62@users.noreply.github.com>
2020-02-11 18:46:42 +0000

    wefqf

---
+++ Insertion_Sort/Insertion_Sort/InsertionSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insertion_Sort
+{
+    class InsertionSort
+    {
+        public void InsertSort(ref int[] theList)
+        {
+            int nextItem, i;
+
+            for (int j = 1; j < theList.Length; j++)
+            {
+                nextItem = theList[j];
+                i = j - 1;
+                while ((i >= 0) && theList[i] > nextItem)
+                {
+                    theList[i + 1] = theList[i];
+                    i = i - 1;
+                }
+                theList[i + 1] = nextItem; // this inserts 
+            }
+        }
+
+        public void DisplayList(int[] aList)
+        {
+            Console.Write("Output of list: ");
+            for (int i = 0; i <= aList.Length - 1; i++)
+            {
+                Console.Write(aList[i] + ", ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
--- Insertion_Sort/Insertion_Sort/Program.cs
+++ Insertion_Sort/Insertion_Sort/Program.cs
@@ -10,12 +10,24 @@ namespace Insertion_Sort
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("This program will read items from a list and then sort them into ascending order.");
+            Console.WriteLine("==================================================================================================");
+            InsertionSort mySorting = new InsertionSort();
+            int[] smallList = { 10, 3, 9, 2, 8, 4, 6, 7, 5, 16, 1, 24, 36, 32, 22, 21, };
+            int[] longList = new int[1000];
+            Random rnd = new Random();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            mySorting.DisplayList(smallList);
+            mySorting.InsertSort(ref smallList);
+            mySorting.DisplayList(smallList);
+
+            for (int i = 0; i < longList.Length; i++)
+            {
+                longList[i] = rnd.Next(1, 101); // creates a number between 1 and 100
+            }
+            //closes program after displaying lsit.
+            Console.WriteLine("The second list is the sorted one. Press enter to terminate the program.");
+            Console.ReadLine();
         }
     }
 }
